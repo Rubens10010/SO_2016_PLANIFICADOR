3e3edfc373bf188d7fbf1f90144c5ced9caafb49
Diego Charrez <ddcharrez@gmail.com>
2016-11-30 16:10:18 -0500

    interfaz

--- App/Qt/Scheduler_SO/mainwindow.cpp
+++ App/Qt/Scheduler_SO/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tabla_tareas->resizeColumnsToContents();
+    ui->tabla_tareas->resizeRowsToContents();
 }
 
 MainWindow::~MainWindow()
